51c4051cc960baeaf44400346fa33f75ff4ca824
scholtm19 <scholtm19@ad.hva.nl>
2024-09-18 18:51:42 +0200

    parallax effect added

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+  document.addEventListener("mousemove", parallax);
+
+  function parallax(e) {
+      var layers = document.querySelectorAll('.layer');
+      Array.prototype.forEach.call(layers, function(layer) {
+          var speed = layer.getAttribute('data-speed');
+  
+          var x = (window.innerWidth - e.pageX * speed) / 100;
+          var y = (window.innerHeight - e.pageY * speed) / 100;
+  
+          layer.style.transform = 'translateX(' + x + 'px) translateY(' + y + 'px)';
+      });
+  }
