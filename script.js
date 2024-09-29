df5f4f2482cb795ec02aa2cc3d23d8f33434587f
scholtm19 <scholtm19@ad.hva.nl>
2024-09-29 17:13:50 +0200

    alt add on colorSelector HTML/JS

--- script.js
+++ script.js
@@ -86,43 +86,43 @@ let topSpeedCounter = document.querySelector("section:nth-of-type(7) video");
 
 
 function changeCarColor_to_red(){
-    document.getElementById("firstImg").src= "img/img-front34.png";
-    document.getElementById("secondImg").src= "img/img-side.png";
-    document.getElementById("thirdImg").src= "img/img-rear.png";
-    document.getElementById("forthImg").src= "img/EXT_Rear_203Qtr.png";
-    document.getElementById("fifthImg").src= "img/img-front.png";
+    document.getElementById("firstImg").src= "img/img-front34.png", document.getElementById("firstImg").alt="Sky view of the front of a mcLaren 750s in the color red";
+    document.getElementById("secondImg").src= "img/img-side.png", document.getElementById("secondImg").alt="The side of a mcLaren 750s in the color red";
+    document.getElementById("thirdImg").src= "img/img-rear.png", document.getElementById("thirdImg").alt="The side of a mcLaren 750s in the color red";
+    document.getElementById("forthImg").src= "img/EXT_Rear_203Qtr.png", document.getElementById("forthImg").alt="The side of a mcLaren 750s in the color red";
+    document.getElementById("fifthImg").src= "img/img-front.png", document.getElementById("fifthImg").alt="The side of a mcLaren 750s in the color red";
 }
 
 function changeCarColor_to_yellow(){
-    document.getElementById("firstImg").src= "img/EXT_Front_203Qtr_1.png";
-    document.getElementById("secondImg").src= "img/EXT_Side.png";
-    document.getElementById("thirdImg").src= "img/EXT_Rear.png";
-    document.getElementById("forthImg").src= "img/EXT_Rear_203Qtr_1.png";
-    document.getElementById("fifthImg").src= "img/EXT_Front.png";
+    document.getElementById("firstImg").src= "img/EXT_Front_203Qtr_1.png", document.getElementById("firstImg").alt="Sky view of the front of a mcLaren 750s in the color yellow";
+    document.getElementById("secondImg").src= "img/EXT_Side.png", document.getElementById("secondImg").alt="The side of a mcLaren 750s in the color yellow";
+    document.getElementById("thirdImg").src= "img/EXT_Rear.png", document.getElementById("thirdImg").alt="The side of a mcLaren 750s in the color yellow";
+    document.getElementById("forthImg").src= "img/EXT_Rear_203Qtr_1.png", document.getElementById("forthImg").alt="The side of a mcLaren 750s in the color yellow";
+    document.getElementById("fifthImg").src= "img/EXT_Front.png", document.getElementById("fifthImg").alt="The side of a mcLaren 750s in the color yellow";
 }
 
 function changeCarColor_to_purple(){
-    document.getElementById("firstImg").src= "img/EXT_Front_203Qtr1.png";
-    document.getElementById("secondImg").src= "img/EXT_Side1.png";
-    document.getElementById("thirdImg").src= "";
-    document.getElementById("forthImg").src= "img/EXT_Rear_203Qtr1.png";
-    document.getElementById("fifthImg").src= "img/EXT_Front1.png";
+    document.getElementById("firstImg").src= "img/EXT_Front_203Qtr1.png", document.getElementById("firstImg").alt="Sky view of the front of a mcLaren 750s in the color purple";
+    document.getElementById("secondImg").src= "img/EXT_Side1.png", document.getElementById("secondImg").alt="The side of a mcLaren 750s in the color purple";
+    document.getElementById("thirdImg").src= "", document.getElementById("thirdImg").alt="The side of a mcLaren 750s in the color red";
+    document.getElementById("forthImg").src= "img/EXT_Rear_203Qtr1.png", document.getElementById("forthImg").alt="The side of a mcLaren 750s in the color purple";
+    document.getElementById("fifthImg").src= "img/EXT_Front1.png", document.getElementById("fifthImg").alt="The side of a mcLaren 750s in the color purple";
 }
 
 function changeCarColor_to_grey(){
-    document.getElementById("firstImg").src= "img/EXT_Front_203Qtr.png";
-    document.getElementById("secondImg").src= "";
-    document.getElementById("thirdImg").src= "";
-    document.getElementById("forthImg").src= "img/EXT_Rear_203Qtr.png";
-    document.getElementById("fifthImg").src= "";
+    document.getElementById("firstImg").src= "img/EXT_Front_203Qtr.png", document.getElementById("firstImg").alt="Sky view of the front of a mcLaren 750s in the color grey";
+    document.getElementById("secondImg").src= "", document.getElementById("secondImg").alt="The side of a mcLaren 750s in the color grey";
+    document.getElementById("thirdImg").src= "", document.getElementById("thirdImg").alt="The side of a mcLaren 750s in the color grey";
+    document.getElementById("forthImg").src= "img/EXT_Rear_203Qtr.png", document.getElementById("forthImg").alt="The side of a mcLaren 750s in the color grey";
+    document.getElementById("fifthImg").src= "", document.getElementById("fifthImg").alt="The side of a mcLaren 750s in the color grey";
 }
 
 function changeCarColor_to_white(){
-    document.getElementById("firstImg").src= "img/EXT_Front_203Qtr3.png";
-    document.getElementById("secondImg").src= "img/EXT_Side3.png";
-    document.getElementById("thirdImg").src= "img/EXT_Rear3.png";
-    document.getElementById("forthImg").src= "img/EXT_Rear_203Qtr3.png";
-    document.getElementById("fifthImg").src= "img/EXT_Front3.png";
+    document.getElementById("firstImg").src= "img/EXT_Front_203Qtr3.png", document.getElementById("firstImg").alt="Sky view of the front of a mcLaren 750s in the color white";
+    document.getElementById("secondImg").src= "img/EXT_Side3.png", document.getElementById("secondImg").alt="The side of a mcLaren 750s in the color white";
+    document.getElementById("thirdImg").src= "img/EXT_Rear3.png", document.getElementById("thirdImg").alt="The side of a mcLaren 750s in the color white";
+    document.getElementById("forthImg").src= "img/EXT_Rear_203Qtr3.png", document.getElementById("forthImg").alt="The side of a mcLaren 750s in the color white";
+    document.getElementById("fifthImg").src= "img/EXT_Front3.png", document.getElementById("fifthImg").alt="The side of a mcLaren 750s in the color white";
 }
 
 // Add nog een class selecter en querySelecter
